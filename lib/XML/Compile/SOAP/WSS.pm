bc273a55f74892f37fc95d6910e99d4e3735b4c4
Mark Overmeer <mark@overmeer.net>
2011-04-27 15:16:01 +0000

    Import of MARKOV/XML-Compile-WSS-0.10 from CPAN.
    gitpan-cpan-distribution: XML-Compile-WSS
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         MARKOV/XML-Compile-WSS-0.10.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

---
+++ lib/XML/Compile/SOAP/WSS.pm
@@ -0,0 +1,42 @@
+# Copyrights 2011 by Mark Overmeer.
+#  For other contributors see ChangeLog.
+# See the manual pages for details on the licensing terms.
+# Pod stripped from pm file by OODoc 2.00.
+use warnings;
+use strict;
+
+package XML::Compile::SOAP::WSS;
+use vars '$VERSION';
+$VERSION = '0.10';
+
+use base 'XML::Compile::WSS', 'XML::Compile::SOAP::Extension';
+
+use Log::Report 'xml-compile-wss';
+
+use XML::Compile::WSS::Util ':wss11';
+#use XML::Compile::SOAP::Util qw/WSDL11/;
+
+
+sub init($)
+{   my ($self, $args) = @_;
+    $self->XML::Compile::WSS::init($args);
+    $self->XML::Compile::SOAP::Extension::init($args);
+}
+
+sub wsdl11Init($$)
+{   my ($self, $wsdl, $args) = @_;
+
+    # When no new(schema) is given, we need to load the schemas now
+    $self->schema || $self->loadSchemas($wsdl);
+}
+
+sub soap11OperationInit($$)
+{   my ($self, $op, $args) = @_;
+
+    trace "adding wss header logic";
+    my $sec = $self->schema->findName('wsse:Security');
+    $op->addHeader(INPUT  => "wsse_Security" => $sec);
+    $op->addHeader(OUTPUT => "wsse_Security" => $sec);
+}
+
+1;
---
+++ lib/XML/Compile/WSS.pm
@@ -0,0 +1,112 @@
+# Copyrights 2011 by Mark Overmeer.
+#  For other contributors see ChangeLog.
+# See the manual pages for details on the licensing terms.
+# Pod stripped from pm file by OODoc 2.00.
+use warnings;
+use strict;
+
+package XML::Compile::WSS;
+use vars '$VERSION';
+$VERSION = '0.10';
+
+
+use Log::Report 'xml-compile-wss';
+
+use XML::Compile::WSS::Util ':wss11';
+use XML::Compile::Util       qw/SCHEMA2001/;
+use XML::Compile::C14N;
+
+use File::Basename           qw/dirname/;
+
+my @prefixes11 = 
+ ( wss  => WSS_11,  wsu    => WSU_10,    wsse  => WSSE_10
+ , ds   => DSIG_NS, dsig11 => DSIG11_NS, dsigm => DSIG_MORE_NS
+ , xenc => XENC_NS, ghc    => GHC_NS,    dsp   => DSP_NS
+ );
+
+my %versions =
+  ( '1.1' => {xsddir => 'wss11', prefixes => \@prefixes11}
+  );
+
+
+sub new(@) { my $class = shift; (bless {}, $class)->init( {@_} ) }
+sub init($)
+{   my ($self, $args) = @_;
+    my $version = $args->{version}
+        or error __x"explicit wss_version required";
+    trace "initializing wss $version";
+
+    $version = '1.1'
+        if $version eq WSS11MODULE;
+
+    $versions{$version}
+        or error __x"unknown wss version {v}, pick from {vs}"
+             , v => $version, vs => [keys %versions];
+    $self->{XCW_version} = $version;
+
+    $self->loadSchemas($args->{schema})
+        if $args->{schema};
+
+    $self;
+}
+
+#-----------
+
+
+sub version() {shift->{XCW_version}}
+sub schema()  {shift->{XCW_schema}}
+
+#-----------
+
+
+sub loadSchemas($)
+{   my ($self, $schema) = @_;
+
+    $schema->isa('XML::Compile::Cache')
+        or error __x"loadSchemas() requires a XML::Compile::Cache object";
+    $self->{XCW_schema} = $schema;
+
+    my $version = $self->version;
+    my $def = $versions{$version};
+
+    my $prefixes = $def->{prefixes};
+    $schema->prefixes(@$prefixes);
+    {   local $" = ',';
+        $schema->addKeyRewrite("PREFIXED(@$prefixes)");
+    }
+
+    (my $xsddir = __FILE__) =~ s! \.pm$ !/$def->{xsddir}!x;
+    my @xsd = glob "$xsddir/*.xsd";
+
+    trace "loading wss for $version";
+
+    $schema->importDefinitions
+       ( \@xsd
+
+         # Missing from wss-secext-1.1.xsd (schema BUG)  Gladly, all
+         # provided schemas have element_form qualified.
+       , element_form_default => 'qualified'
+       );
+
+    # Another schema bug; attribute wsu:Id not declared qualified
+    my ($wsu, $xsd) = (WSU_10, SCHEMA2001);
+    $schema->importDefinitions( <<__PATCH );
+<schema
+  xmlns="$xsd"
+  targetNamespace="$wsu"
+  elementFormDefault="qualified"
+  attributeFormDefault="qualified">
+    <attribute name="Id" type="ID" />
+</schema>
+__PATCH
+
+    XML::Compile::C14N->new(version => 1.1, schema => $schema);
+    $schema->allowUndeclared(1);
+    $schema->addCompileOptions(RW => mixed_elements => 'STRUCTURAL');
+    $schema->anyElement('ATTEMPT');
+
+    $self;
+}
+
+
+1;
